e2053cd8a5df6015fa44e168b076927e576869e2
Rostislav <rostik057@gmail.com>
2025-06-16 21:31:40 +0300

    !fix

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy => 
             {
-                policy.WithOrigins(["http://localhost", "http://localhost:3000", "https://my-financier.ru"])
+                policy.WithOrigins(["https://localhost", "http://localhost:3000", "https://my-financier.ru"])
                     .AllowAnyHeader()
                     .AllowAnyMethod();
         }
